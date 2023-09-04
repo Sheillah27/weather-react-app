d880064b2cfc12067c45026d8c84548407e42a1e
Sheillah Wafula <130930119+Sheillah27@users.noreply.github.com>
2023-09-04 23:50:21 +0300

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import "./App.css";
-
 function App() {
   return (
     <div className="App">
       <h2>React Weather App</h2>
-      <button className="btn-btn-primary">Hello</button>
+
+      <form>
+        <input type="search" placeholder="Type City" />
+
+        <input type="submit" value="Search" />
+      </form>
+      <div className="Container">
+        <div className="row">
+          <div className="Col-6">
+            <img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" />
+            <span>26 ℃</span>
+            <div>Monday 4th 3:40 pm</div>
+            <div>Description</div>
+          </div>
+        </div>
+      </div>
+      <div classNameclassName="col 6">
+        <ul>
+          <li>Humidity</li>
+          <li>Wind speed</li>
+        </ul>
+      </div>
     </div>
   );
 }
