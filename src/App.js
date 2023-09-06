d87885ef7233d8338e50f9c02cf3f7882448cd04
Sheillah Wafula <130930119+Sheillah27@users.noreply.github.com>
2023-09-06 11:44:02 +0300

    Added app html

--- src/App.js
+++ src/App.js
@@ -1,32 +1,44 @@
 import "./App.css";
+import "./Weather.css";
+
 function App() {
   return (
-    <div className="App">
-      <h2>React Weather App</h2>
-
+    <div className="climate">
       <form>
-        <input type="search" placeholder="Type City" />
-
-        <input type="submit" value="Search" />
-      </form>
-      <div className="Container">
         <div className="row">
-          <div className="Col-6">
-            <img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" />
-            <span>26 ℃</span>
-            <div>Monday 4th 3:40 pm</div>
-            <div>Description</div>
+          <div className="col-9">
+            <input
+              type="text"
+              placeholder=" Enter city"
+              className="form-control"
+            />
+          </div>
+          <div className="col-3">
+            <input type="Submit" value=" Seach" className="btn btn-primary" />
           </div>
         </div>
-      </div>
-      <div classNameclassName="col 6">
-        <ul>
-          <li>Humidity</li>
-          <li>Wind speed</li>
-        </ul>
+      </form>
+      <h1>Nairobi</h1>
+      <ul>
+        <li>Wed 10 am </li>
+        <li> Broken clouds</li>
+      </ul>
+      <div className="row">
+        <div className="col-6">
+          <img
+            src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+            alt="broken clouds"
+          />{" "}
+          25 ℃
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Humidity:44%</li>
+            <li>Wind :16 Km/h</li>
+          </ul>
+        </div>
       </div>
     </div>
   );
 }
-
 export default App;
